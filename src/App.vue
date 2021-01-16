7364392784e6c2ab0e3481ed4449bfee0a3d5b5a
liucheng612953 <a6129536@163.com>
2021-01-16 14:39:59 +0800

    1.10tianqi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+     <el-select v-model="value" placeholder="请选择">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
   </div>
 </template>
-
+<script>
+  export default {
+    data() {
+      return {
+        options: [{
+          value: '选项1',
+          label: '+'
+        }, {
+          value: '选项2',
+          label: '-'
+        }, {
+          value: '选项3',
+          label: '*'
+        }, {
+          value: '选项4',
+          label: '/'
+        }, {
+          value: '选项5',
+          label: ''
+        }],
+        value: ''
+      }
+    }
+  }
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
